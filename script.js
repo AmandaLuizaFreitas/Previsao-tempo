dbe3ce28014baeadb199d10a46a5b8d2e8404060
Amanda <110351770+AmandaLuizaFreitas@users.noreply.github.com>
2023-09-19 11:08:20 -0300

    first commit

---
+++ script.js
@@ -0,0 +1,85 @@
+document.querySelector('.busca').addEventListener('submit', async(event)=>{
+ 
+  event.preventDefault() 
+  let input = document.querySelector('#searchInput').value
+ 
+  if(input !== ''){
+    clearInfo()
+   showWarning('carregando...')
+    let url = `https://api.openweathermap.org/data/2.5/weather?q=${encodeURI(input)}&lon={lon}&appid=5bf0d2258b2969b795e8fd0836711a1b&units=metric&lang=pt_br`
+ 
+    let results = await fetch(url)
+    let json=  await results.json()
+     
+ 
+     if(json.cod=== 200){
+      
+       showInfo({
+          name:json.name,
+          country:json.sys.country,
+          temp:json.main.temp,
+          tempIcon:json.weather[0].icon,
+          windSpeed:json.wind.speed,
+          windAngle:json.wind.deg,
+          humidity:json.main.humidity
+         
+       })
+       
+       switch (json.weather[0].main) {
+        case 'Clear':
+            image.src = 'images/clear.png';
+            break;
+
+        case 'Rain':
+            image.src = 'images/rain.png';
+            break;
+
+        case 'Snow':
+            image.src = 'img/snow.png';
+            break;
+
+        case 'Clouds':
+            image.src = 'img/cloud.png';
+            break;
+
+        case 'Haze':
+            image.src = 'img/mist.png';
+            break;
+
+        default:
+            image.src = '';
+    }
+     }
+ 
+     else{
+       clearInfo()
+       showWarning('Não encontramos esta localização')
+     }
+ }
+ else{
+    clearInfo()
+  }
+  
+ })
+ 
+ function showInfo(json){
+  showWarning('')
+  document.querySelector('.resultado').style.display='block'
+  document.querySelector('.titulo').innerHTML = `${json.name} , ${json.country}`
+  document.querySelector('.tempInfo').innerHTML = `${parseInt(json.temp)} <sup>ºC</sup>`
+  document.querySelector('.humidity-info').innerHTML=`${json.humidity}%`
+  document.querySelector('.ventoInfo').innerHTML = `${parseInt(json.windSpeed)} <span>km/h</span>`
+ 
+  document.querySelector('.temp img').setAttribute('src',`http://openweathermap.org/img/wn/${json.tempIcon}@2x.png`)
+  document.querySelector('.ventoPonto').style.transform=`rotate(${json.windAngle-90}deg) `
+ }
+ function clearInfo(){
+    showWarning('')
+    document.querySelector('.resultado').style.display='none'
+ }
+ 
+ function showWarning(msg){
+  document.querySelector('.aviso').innerHTML= msg
+
+
+ }
