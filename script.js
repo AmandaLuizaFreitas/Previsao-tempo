ffaca2fdda5777de59c5a4c9cf14e11adba60dea
Amanda <110351770+AmandaLuizaFreitas@users.noreply.github.com>
2023-09-24 17:13:21 -0300

    Atualização

--- script.js
+++ script.js
@@ -6,13 +6,14 @@ document.querySelector('.busca').addEventListener('submit', async(event)=>{
   if(input !== ''){
     clearInfo()
    showWarning('carregando...')
-    let url = `https://api.openweathermap.org/data/2.5/weather?q=${encodeURI(input)}&lon={lon}&appid=5bf0d2258b2969b795e8fd0836711a1b&units=metric&lang=pt_br`
+   
+    let url = `https://api.openweathermap.org/data/2.5/weather?q=${encodeURI(input)}&lon={lon}&appid=5bf0d2258b2969b795e8fd0836711a1b&units=metric&lang=pt_br`;
  
     let results = await fetch(url)
     let json=  await results.json()
      
  
-     if(json.cod === 200){
+     if(json.cod=== 200){
       
        showInfo({
           name:json.name,
@@ -22,17 +23,16 @@ document.querySelector('.busca').addEventListener('submit', async(event)=>{
           windSpeed:json.wind.speed,
           windAngle:json.wind.deg,
           humidity:json.main.humidity
-         
        })
-       
-    
      }
  
      else{
        clearInfo()
        showWarning('Não encontramos esta localização')
-
+       
      }
+
+    
  }
  else{
     clearInfo()
@@ -46,10 +46,9 @@ document.querySelector('.busca').addEventListener('submit', async(event)=>{
   document.querySelector('.titulo').innerHTML = `${json.name} , ${json.country}`
   document.querySelector('.tempInfo').innerHTML = `${parseInt(json.temp)} <sup>ºC</sup>`
   document.querySelector('.humidity-info').innerHTML=`${json.humidity}%`
-  document.querySelector('.ventoInfo').innerHTML = `${parseInt(json.windSpeed)} <span>km/h</span>`
+  document.querySelector('.ventoInfo').innerHTML = `${json.windSpeed} <span>km/h</span>`
  
   document.querySelector('.temp img').setAttribute('src',`http://openweathermap.org/img/wn/${json.tempIcon}@2x.png`)
-  document.querySelector('.ventoPonto').style.transform=`rotate(${json.windAngle-90}deg) `
  }
  function clearInfo(){
     showWarning('')
@@ -58,6 +57,4 @@ document.querySelector('.busca').addEventListener('submit', async(event)=>{
  
  function showWarning(msg){
   document.querySelector('.aviso').innerHTML= msg
-
-
  }
